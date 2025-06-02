b59841ea8a2f8b86f1038a111ef93461d5001d33
jesvaca <jjvaca68@gmail.com>
2025-06-02 08:47:48 -0600

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jesvaca/godesde0
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jesvaca/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
